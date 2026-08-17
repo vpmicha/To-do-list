ae15f13bb5ad94093d30f14ba485f59b24f8b30c
Vasilis Michalopoulos <vpmicha@gmail.com>
2026-08-18 02:59:18 +0300

    fixed small bug in innit method and added a method that allows adding tasks in to do list.

--- To-do-list.py
+++ To-do-list.py
@@ -2,6 +2,13 @@ import json
 import sys
 
 class To_do_list():
-    def __init__(self, task):
+    def __init__(self):
         self.task = {}
+    def add_task(self,task):
+        if task not in self.task:
+            self.task[task] = False
+        else:
+            print('Task already in to-do list')
+            
+        
 
