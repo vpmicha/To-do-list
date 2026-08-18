810bb2e9abc102ae6e829248136f7749947bfa8b
vpmicha <vpmicha@gmail.com>
2026-08-18 03:12:33 +0300

    added a remove task method and fixed a syntax mistake in line 5

--- To-do-list.py
+++ To-do-list.py
@@ -3,19 +3,27 @@ import sys
 
 class To_do_list():
     def __init__(self):
-    def __init__(self):
-        self.task = {}
+        self.tasks = {}
+
     def add_task(self,task):
-        if task not in self.task:
-            self.task[task] = False
+        if task not in self.tasks:
+            self.tasks[task] = False
         else:
             print('Task already in to-do list')
             
-    def add_task(self,task):
-        if task not in self.task:
-            self.task[task] = False
+    def add_task(self, task):
+        if task not in self.tasks:
+            self.task[task] = 'False'
         else:
             print('Task already in to-do list')
+
+    def remove_task(self, task):
+        if task in self.tasks:
+            del self.tasks[task]
+        else:
+            print('Task not found')
+            
+        
             
         
 
