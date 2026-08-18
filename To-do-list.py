9787f8a7821931fff5a5632954ba63aa93f9cca7
vpmicha <vpmicha@gmail.com>
2026-08-18 03:16:48 +0300

    added a complete method so it marks as completed, completed tasks

--- To-do-list.py
+++ To-do-list.py
@@ -7,22 +7,28 @@ class To_do_list():
 
     def add_task(self,task):
         if task not in self.tasks:
-            self.tasks[task] = False
+            self.tasks[task.lower()] = False
         else:
             print('Task already in to-do list')
             
     def add_task(self, task):
-        if task not in self.tasks:
-            self.task[task] = 'False'
+        if task.lower() not in self.tasks:
+            self.task[task.lower()] = 'Not Done'
         else:
             print('Task already in to-do list')
 
     def remove_task(self, task):
-        if task in self.tasks:
-            del self.tasks[task]
+        if task.lower() in self.tasks:
+            del self.tasks[task.lower()]
         else:
             print('Task not found')
-            
+
+    def complete(self, task):
+        if task.lower() in self.tasks:
+            self.tasks[task.lower()] = 'Done'
+        else:
+            print('Could not mark as done since task does not exist')
+
         
             
         
