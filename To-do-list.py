de7a692c51419a141a72665a632f33861ca21d18
vpmicha <vpmicha@gmail.com>
2026-08-18 03:39:24 +0300

    last minute changes

--- To-do-list.py
+++ To-do-list.py
@@ -1,9 +1,11 @@
 import json
+from pickle import NONE
 
 class To_do_list():
     def __init__(self):
-        self.tasks = {}
-
+        with open('To do list', 'r') as file:
+            self.task = json.load(file)
+            
     def add_task(self,task):
         if task.lower() not in self.tasks:
             self.tasks[task.lower()] = 'Not Done'
@@ -30,11 +32,18 @@ class To_do_list():
     
     def __str__(self):
         with open('To do list', 'r') as file:
-            list = json.load(file)
-            return str(list)
+            todo = json.load(file)
+            return str(todo)
 
-    
+todo = To_do_list()
+todo.add_task('Learn Git and GitHub')
+todo.add_task('Learn Machine learning')
+todo.add_task('Learn Python')
 
+todo.remove_task('Learn Machine learning')
+todo.complete('Learn Git and GitHub')
+
+print(todo)
         
             
         
