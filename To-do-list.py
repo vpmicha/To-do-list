0b09105dbd30ba19e81f26a86a8bb9c07313dec4
vpmicha <vpmicha@gmail.com>
2026-08-18 03:32:59 +0300

    added a complete str method that returns the whole list and managed to make it save the list in a json file

--- To-do-list.py
+++ To-do-list.py
@@ -1,33 +1,39 @@
 import json
-import sys
 
 class To_do_list():
     def __init__(self):
         self.tasks = {}
 
     def add_task(self,task):
-        if task not in self.tasks:
-            self.tasks[task.lower()] = False
+        if task.lower() not in self.tasks:
+            self.tasks[task.lower()] = 'Not Done'
+            with open('To do list', 'w') as file:
+                json.dump(self.tasks, file)
         else:
             print('Task already in to-do list')
             
-    def add_task(self, task):
-        if task.lower() not in self.tasks:
-            self.task[task.lower()] = 'Not Done'
-        else:
-            print('Task already in to-do list')
-
     def remove_task(self, task):
         if task.lower() in self.tasks:
             del self.tasks[task.lower()]
+            with open('To do list', 'w') as file:
+                json.dump(self.tasks, file)
         else:
             print('Task not found')
 
     def complete(self, task):
         if task.lower() in self.tasks:
             self.tasks[task.lower()] = 'Done'
+            with open('To do list', 'w') as file:
+                json.dump(self.tasks, file)
         else:
             print('Could not mark as done since task does not exist')
+    
+    def __str__(self):
+        with open('To do list', 'r') as file:
+            list = json.load(file)
+            return str(list)
+
+    
 
         
             
