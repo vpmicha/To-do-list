19cf4ea1605a553c86c38306c7e9a4ac1340b2f4
vpmicha <vpmicha@gmail.com>
2026-08-18 03:07:54 +0300

    nothing

--- To-do-list.py
+++ To-do-list.py
@@ -3,9 +3,19 @@ import sys
 
 class To_do_list():
     def __init__(self):
+    def __init__(self):
         self.task = {}
     def add_task(self,task):
         if task not in self.task:
             self.task[task] = False
         else:
             print('Task already in to-do list')
+            
+    def add_task(self,task):
+        if task not in self.task:
+            self.task[task] = False
+        else:
+            print('Task already in to-do list')
+            
+        
+
